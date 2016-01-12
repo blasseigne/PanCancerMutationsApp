c8ae009945aff868d761b3ca528a0ab0c44e5ebe
blasseigne <brittany.lasseigne@gmail.com>
2016-01-12 09:04:01 -0600

    First version of Kandoth mutation analysis app.

---
+++ server.R
@@ -0,0 +1,86 @@
+library(shiny)
+library(plyr)
+load("kandothAnno.Rdata")
+load("numBySample.Rdata")
+
+# Define server logic required to draw a histogram
+shinyServer(function(input, output) {
+  
+  # Expression that generates a histogram. The expression is
+  # wrapped in a call to renderPlot to indicate that:
+  #
+  #  1) It is "reactive" and therefore should re-execute automatically
+  #     when inputs change
+  #  2) Its output type is a plot
+  
+  
+  #Returns the requested data for the CADD distribution histogram
+  datasetInput <- reactive({
+    switch(input$distributionData,
+           "All"=na.omit(kandothAnno2$v13CADDscaled),
+           "Missense"=na.omit(kandothAnno2[kandothAnno2$Variant_Classification=="Missense",]$v13CADDscaled), 
+           "Nonsense"=na.omit(kandothAnno2[kandothAnno2$Variant_Classification=="Nonsense",]$v13CADDscaled), 
+           "Nonstop"=na.omit(kandothAnno2[kandothAnno2$Variant_Classification=="Nonstop",]$v13CADDscaled), 
+           "RNA"=na.omit(kandothAnno2[kandothAnno2$Variant_Classification=="RNA",]$v13CADDscaled), 
+           "Silent"=na.omit(kandothAnno2[kandothAnno2$Variant_Classification=="Silent",]$v13CADDscaled), 
+           "Splice Site"=na.omit(kandothAnno2[kandothAnno2$Variant_Classification=="Splice_Site",]$v13CADDscaled))
+  })
+  #generate summary of the distribution
+  output$summary<-renderPrint({
+    distributionData<-datasetInput()
+    summary(distributionData)
+  })
+  
+  #CADD distribution histogram
+  output$scaledCADD <- renderPlot({
+    distributionData    <- datasetInput()  # R object in loaded data
+    bins <- seq(min(distributionData), max(distributionData), length.out = input$bins + 1)
+    # draw the histogram with the specified number of bins
+    hist(distributionData, breaks=bins, col = 'darkgray', border = 'white', xlab="CADD", main="CADD Mutation Frequency")
+  })
+  
+  #Variant Classification barplot
+  mytable<-table(kandothAnno2$Variant_Classification)
+  names(mytable)[2]<-"Frameshift_Del"
+  names(mytable)[1]<-"NA"
+  names(mytable)[3]<-"Frameshift_Ins"
+  names(mytable)[4]<-"InframeDel"
+  names(mytable)[5]<-"InframeIns"
+  dfTable<-as.data.frame(mytable)
+  dfTable<-dfTable[order(-dfTable$Freq),]
+  library(ggplot2)
+  output$bp<-renderPlot({ggplot(dfTable, aes(x="", y=Freq, fill=Var1))+
+      geom_bar(width=1, colour="black", stat="identity") + scale_fill_manual(values=c("white", "orange", "lightblue", "black", "gray", "red", "blue", "pink", "purple", "green", "yellow"))+
+      labs(x="Variant Classification", y="Variant Frequency")})
+  
+  #Samples by Cancer Pie Chart
+  numByCancer<-table(numBySample$V3)
+  numByCancerDF<-as.data.frame(numByCancer)
+  #pie chart
+  output$pieCancer<-renderPlot({ggplot(numByCancerDF, aes(x="", y=Freq, fill=Var1))+
+      geom_bar(width=1, stat="identity")+
+      theme(panel.background=element_blank())+
+      coord_polar("y", start=0)+
+      theme(axis.text.x=element_blank())+
+      scale_fill_brewer(palette="Set3")+
+      scale_x_discrete(name="")+
+      scale_y_discrete(name="")})
+  
+  #numBySample is the number of mutations by each sample, annotated with tumor type
+  small<-subset(numBySample, numBySample$Freq<3000)
+  output$bp4<-renderPlot({ggplot(small, aes(V3, Freq))+
+      geom_jitter()+
+      theme(axis.text.x=element_text(angle=90, hjust=1))+
+      labs(x="Cancer", y="# of Variants per Sample")})
+  
+  output$variantCount<-renderText({paste("Total Number of Variants:", nrow(kandothAnno2))})
+  
+  output$sampleCount<-renderText({paste("Total Number of Samples:", length(unique(kandothAnno2$ShortTumor_Sample)))})
+  
+  output$cancerCount<-renderText({paste("Number of Cancers Represented:", length(unique(kandothAnno2$Cancer))-1)}) #-1 accounts for "NA"
+  
+  output$cancerSummary<-renderDataTable({count(kandothAnno2, 'Cancer')})
+  
+  geneCount<-count(kandothAnno2, 'Gene.x')
+  output$geneSummary<-renderDataTable({geneCount[order(-geneCount$freq),]}, options=list(lengthMenu=c(10, 20, 30, 40, 50), pageLength=10))
+})
---
+++ ui.R
@@ -0,0 +1,76 @@
+library(shiny)
+
+# Define UI for application that draws a histogram
+shinyUI(fluidPage(
+  # Application title
+  titlePanel(
+    h1("TCGA PanCancer Somatic Mutations", align="center")),
+  
+  # Sidebar, information about where variants came from, etc.--should we specify this is fro the example dataset?
+  sidebarLayout(
+    sidebarPanel(
+      h5("TCGA PanCancer Somatic Mutation Data"), 
+      p("Variant data was accessed from:"), 
+      a("Kandoth, et al. 2013", href="http://www.nature.com/nature/journal/v502/n7471/full/nature12634.html"), 
+      br(),
+      br(),
+      p("These variants were annotated with the HudsonAlpha Institute for Biotechnology Variant Annotation pipeline and v1.3 CADD scores"), 
+      br(), 
+      h5("More information about CADD can be found here:"),
+      a("Combined Annotation Dependent Depletion (CADD)", href="http://cadd.gs.washington.edu"), 
+      br(),
+      a("Kircher & Witten, et al. 2014", href="http://www.nature.com/ng/journal/v46/n3/full/ng.2892.html"),
+      br(), 
+      br(), 
+      br(), 
+      br(),
+      img(src = "hudson-logo.png", height=50, width=200)
+    ),
+    
+    # Set up tabs for the main panel
+    mainPanel(
+      tabsetPanel(
+        tabPanel("Summary", 
+                 br(), 
+                 fluidRow(
+                   column(12, align="center",
+                          textOutput("variantCount"), 
+                          textOutput("sampleCount"), 
+                          textOutput("cancerCount"), 
+                          br()
+                   )
+                 ),
+                 fluidRow(
+                   column(5,
+                          h5("Variant Classification in Dataset"),
+                          plotOutput("bp") 
+                   ),
+                   column(6, offset=1,
+                          h5("Samples per Cancer in Dataset"),
+                          plotOutput("pieCancer")
+                   ))
+        ),
+        tabPanel("Cancer Summary", 
+                 dataTableOutput("cancerSummary")),
+        tabPanel("Variants per Sample by Cancer", 
+                 plotOutput("bp4"),
+                 br(),
+                 p("*Samples with 3000+ variants were removed for this analysis")), 
+        tabPanel("CADD Summary", plotOutput("scaledCADD"),
+                 hr(),
+                 fluidRow(
+                   column(2, 
+                          selectInput("distributionData",
+                                      label="Please select variant classification to examine:",
+                                      choices=list("All", "Missense", "Nonsense", "Nonstop", "RNA", "Silent", "Splice Site"),
+                                      selected="All")),
+                   column(2, offset=0.5, 
+                          sliderInput("bins", "Number of bins:", min=1, max=50, value=30)),
+                   column(7, offset=0.5,
+                          h4("Variant Summary:"),
+                          verbatimTextOutput("summary")))),
+        tabPanel("Genes", 
+                 dataTableOutput("geneSummary"))
+      )
+    ))
+))
