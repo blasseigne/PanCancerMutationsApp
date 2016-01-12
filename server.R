47e91e6fd8bd31d8c81241c61d55e541a755e102
blasseigne <brittany.lasseigne@gmail.com>
2016-01-12 09:15:53 -0600

    more organizing

--- server.R
+++ server.R
@@ -6,14 +6,7 @@ load("numBySample.Rdata")
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
   
-  # Expression that generates a histogram. The expression is
-  # wrapped in a call to renderPlot to indicate that:
-  #
-  #  1) It is "reactive" and therefore should re-execute automatically
-  #     when inputs change
-  #  2) Its output type is a plot
-  
-  
+#for "CADD Summary" Tab  
 #Returns the requested data for the CADD distribution histogram
   datasetInput <- reactive({
     switch(input$distributionData,
